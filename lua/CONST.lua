7715cfa37f614ab0ad5ac234719ca765b05f73ec
potasium142 <potasium.1024@gmail.com>
2024-03-22 16:45:40 +0700

    fix cmp

--- lua/CONST.lua
+++ lua/CONST.lua
@@ -2,9 +2,10 @@ local WIN32 = (jit.os == "Windows")
 
 local M = {}
 
+local LOCAL_SYSTEM_MONOSPACE_FONT = os.getenv("MONOSPACE_FONT") or "Consolas"
 local LOCAL_SYSTEM_FONT_SIZE = os.getenv("MONOSPACE_FONT_SIZE")
 M.FONT_SIZE = LOCAL_SYSTEM_FONT_SIZE or WIN32 and 15 or 13
-M.FONTS = M.LOCAL_SYSTEM_MONOSPACE_FONT .. ",Symbols Nerd Font Mono:h"
+M.FONTS = LOCAL_SYSTEM_MONOSPACE_FONT .. ",Symbols Nerd Font Mono:h"
 M.FONT_WEIGHT = os.getenv("MONOSPACE_FONT_WEIGHT") or "Medium"
 
 return M
