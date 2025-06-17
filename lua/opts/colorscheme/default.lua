845a5b31d2f3d2ede8e2ba365713bcc72f0e7aae
potasium <potasium.1024@gmail.com>
2025-06-17 18:32:54 +0700

    change default colorscheme

--- lua/opts/colorscheme/default.lua
+++ lua/opts/colorscheme/default.lua
@@ -1,9 +1,23 @@
 return {
+	-- {
+	-- 	"RRethy/base16-nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("base16-everforest")
+	-- 	end,
+	-- },
 	{
-		"RRethy/base16-nvim",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("base16-monokai")
+
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function(_, opts)
+			local everforest = require("everforest")
+
+			everforest.setup(opts)
+			everforest.load()
 		end,
 	},
 }
