9c02b405ccad6b04d471251651803cafa6694b2a
potasium142 <potasium.1024@gmail.com>
2025-07-08 07:12:19 +0700

    tweak blink

--- lua/plugins/cmp/blink.lua
+++ lua/plugins/cmp/blink.lua
@@ -3,50 +3,70 @@ return {
 	{
 		"saghen/blink.cmp",
 		version = "1.*",
-		opts = {
-			keymap = {
-				preset = "enter",
-				["<Tab>"] = {
-					"snippet_forward",
-					"select_next",
-					"fallback",
-				},
-				["<S-Tab>"] = {
-					"snippet_backward",
-					"select_prev",
-					"fallback",
-				},
-				["<C-k>"] = {
-					"scroll_documentation_up",
-					"fallback",
-				},
-				["<C-j>"] = {
-					"scroll_documentation_down",
-					"fallback",
-				},
-				["<ESC>"] = { "hide", "fallback" },
-			},
-			signature = { enabled = true },
-			completion = {
-				list = {
-					selection = {
-						preselect = true,
+		opts = function()
+			return {
+				keymap = {
+					preset = "none",
+					["<C-space>"] = {
+						"show",
+						"show_documentation",
+						"hide_documentation",
 					},
+					["<Enter>"] = {
+						"select_and_accept",
+						"fallback",
+					},
+					["<Tab>"] = {
+						function(cmp)
+							if cmp.snippet_active() then
+								return cmp.select_next()
+							end
+						end,
+						"snippet_forward",
+						"select_next",
+						"fallback",
+					},
+					["<S-Tab>"] = {
+						"snippet_backward",
+						"select_prev",
+						"fallback",
+					},
+					["<C-k>"] = {
+						"scroll_documentation_up",
+						"fallback",
+					},
+					["<C-j>"] = {
+						"scroll_documentation_down",
+						"fallback",
+					},
+					["<ESC>"] = { "hide", "fallback" },
 				},
-				menu = {
-					draw = {
-						columns = {
-							{
-								"kind_icon",
-							},
-							{
-								"label",
+				signature = { enabled = true },
+				completion = {
+					list = {
+						selection = {
+							preselect = function(ctx)
+								return not require("blink.cmp").snippet_active({ direction = 1 })
+							end,
+
+							auto_insert = false,
+						},
+					},
+					menu = {
+						draw = {
+							columns = {
+								{
+									"kind_icon",
+								},
+								{
+									"label",
+								},
 							},
 						},
 					},
 				},
-			},
-			appearance = { kind_icons = icons },
-		},
+				appearance = { kind_icons = icons },
+			}
+		end,
 	},
 }
